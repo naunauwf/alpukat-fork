496ff41c404bfed91ad05ccab56f93026bcacb4d
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-06-25 14:25:54 +0700

    feat: add looping.ts from naunauwf

---
+++ looping.ts
@@ -0,0 +1,19 @@
+for (let i: number = 0; i < 10; i++) {
+  console.log("Hello ALpukat! ");
+}
+
+let i: number = 0;
+while (i < 10) {
+  console.log(`loop ke-${i}`);
+  i++;
+}
+
+do {
+  console.log("Hello ALpukat!");
+  i++;
+} while (i < 10);
+
+const mobils: string[] = ["Toyota", "Honda", "Yamaha"];
+for (const mobil of mobils) {
+  console.log(mobil);
+}
